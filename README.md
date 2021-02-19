675974c359212c8732b99eaa3f085b8badf018be
junxu <junxu@junxudeMacBook-Air.local>
2021-02-19 16:14:04 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test
+I'm Main
---
+++ run.sh
@@ -0,0 +1,10 @@
+git status
+read -n2 -p "add all and then commit [Y/N]?" answer
+case $answer in
+(N | n)
+    echo "Bye";;
+(*)
+    git add -A;
+    git commit -m "update";
+    git push origin master;;
+esac
